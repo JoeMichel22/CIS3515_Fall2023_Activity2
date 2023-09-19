00a62a92935f6054756091af299d268bac4e43b9
Joe Michel <tun69277@temple.edu>
2023-09-19 15:20:14 -0400

    Added onItemSelectedListener to change the size of the textview to match the selected number in the array

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -2,8 +2,10 @@ package edu.temple.inclassuiacvitivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.*
+import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,14 +16,26 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* Step 1: Populate this array */
-        val numberArray = Array<Int>(10){ (it + 10)}
+        val numberArray = Array<Int>(50){ (it + 10)}
 
         /* Step 2: Create adapter to display items from array in Spinner */
         spinner.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, numberArray)
 
 
         // Step 3: Change TextView's text size to the number selected in the Spinner */
-        spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object: OnItemSelectedListener,
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                p0?.run {
+                    val size= getItemAtPosition(p2).toString()
+
+                    displayTextView.textSize= size.toFloat()
+                }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {            }
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {            }
+        }
 
     }
 }
